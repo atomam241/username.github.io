e9998da83c29351ef7c780cf7abd791f36f6e402
vargas <omcclu@gmail.com>
2018-11-01 11:01:45 -0400

    update for class of 2019

--- countdown/js/index.js
+++ countdown/js/index.js
@@ -1,5 +1,5 @@
 var last = 1;
-var end = new Date(2018, 5, 1, 17, 30);
+var end = new Date(2019, 5, 1, 17, 31);
 Date.daysBetween = function(date1, date2) {
   //Get 1 day in milliseconds
   var one_day = 1000 * 60 * 60 * 24;
