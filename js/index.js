74497b3ded794c3c71c3768e5eb801b71b1b4e50
odm7341 <omcclu@gmail.com>
2019-01-02 17:42:23 -0500

    bug fixes

--- js/index.js
+++ js/index.js
@@ -4,32 +4,39 @@ canvas.width = document.body.clientWidth;
 canvas.height = document.body.clientHeight;
 
 var stars = {};
-var adjustmentPoint = [0, 0];
-
+var adjustmentPoint = [0, 2];
+var overshoot = 500;
 var mousecoords = [];
 
 var DENSITY = 5; // "per square inch" or something maybe
 makeStars(DENSITY);
 
-$("#starfield").mousemove(function(evt) {
-  mousecoords = [evt.clientX, evt.clientY];
+
+
+function getCoords(event) {
+  mousecoords = [event.clientX, event.clientY];
   adjustmentPoint = [
     (mousecoords[0] - canvas.width / 2) / 100,
     (mousecoords[1] - canvas.height / 2) / 100
   ];
-});
+}
+
+
+requestAnimationFrame(drawStars);
+
 
 function makeStars(DENSITY) {
   var totalStars =
-    Math.floor(canvas.width / 72) * Math.floor(canvas.height / 72) * DENSITY;
+    Math.floor((canvas.width + overshoot) / 72) * Math.floor((canvas.height + overshoot) / 72) * DENSITY;
 
   var randomX, randomY, randomZ;
   var sortable = [];
   for (var i = 0; i < totalStars; i++) {
-    randomX = Math.random() * (canvas.width - 1) + 1;
-    randomY = Math.random() * (canvas.height - 1) + 1;
-    randomZ = Math.random() * 5;
-    stars[i] = [randomX, randomY, randomZ];
+    randomX = Math.random() * (canvas.width - 1 + overshoot) + 1 - overshoot/2;
+    randomY = Math.random() * (canvas.height - 1 + overshoot) + 1 - overshoot/2;
+    randomZ = Math.random() * 10;
+    color = randColor();
+    stars[i] = [randomX, randomY, randomZ, color, randomX, randomY];
     sortable.push(randomZ);
   }
   sortable.sort();
@@ -39,33 +46,41 @@ function makeStars(DENSITY) {
   }
 }
 
-requestAnimationFrame(drawStars);
+
 function drawStars() {
   context.clearRect(0, 0, canvas.width, canvas.height);
-  context.fillStyle = "#FF0000";
+  //context.fillStyle = "#FF0000";
   for (var i in stars) {
-    context.fillStyle = randColor();
+    context.fillStyle = stars[i][3];
     context.fillRect(stars[i][0], stars[i][1], stars[i][2], stars[i][2]);
+    context.fill();
   }
   updateStars();
   requestAnimationFrame(drawStars);
 }
+
 function updateStars() {
   for (var i in stars) {
+
+    // stars[i][0] = stars[i][4] + adjustmentPoint[0] * stars[i][2] / 10;
+    // stars[i][1] = stars[i][5] + adjustmentPoint[1] * stars[i][2] / 10;
+
     stars[i][0] += adjustmentPoint[0] * stars[i][2] / 10;
     stars[i][1] += adjustmentPoint[1] * stars[i][2] / 10;
-    if (stars[i][0] >= canvas.width) {
-      stars[i][0] = -5;
+
+    if (stars[i][0] >= canvas.width + overshoot/2) {
+      stars[i][0] = -10;
     }
-    if (stars[i][1] >= canvas.height) {
-      stars[i][1] = -5;
+    if (stars[i][1] >= canvas.height + overshoot/2) {
+      stars[i][1] = -10;
     }
-    if (stars[i][0] < -6) {
+    if (stars[i][0] < -overshoot/2) {
       stars[i][0] = canvas.width;
     }
-    if (stars[i][1] < -6) {
+    if (stars[i][1] < -overshoot/2) {
       stars[i][1] = canvas.height;
     }
+
   }
 }
 
