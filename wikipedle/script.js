0cdd7366ee8731f5b0eb6c6bffdb45b4e86c93cb
Owen McClure <omcclu@gmail.com>
2022-05-18 01:54:20 -0400

    Update script.js

--- wikipedle/script.js
+++ wikipedle/script.js
@@ -224,18 +224,17 @@ function show_all_clues(numRed){
 			$("#clue" + i).css({
 				color: "#f22"
 			});						
-		}if (i == numRed){
+		}else if (i == numRed){
 			$("#clue" + i).css({
 				color: "#ff2"
 			});
 		}else {
-			add_clue(i, clues[i]);
 			$("#clue" + i).css({
 				opacity: 1,
 				color: "#2f2"
 			});
 		}
-
+		add_clue(i, clues[i]);
 		
 	}
 }
